de8ff35e5b513eacf9bdb92fdd44879efe7e2b26
yday <yangdayeon2@gmail.com>
2022-03-18 12:14:57 +0900

    임의의 난수를 배열에 넣고 합계와 평균을 구하기

--- sumAvg.c
+++ sumAvg.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
 	int a[10];
-	for (int i = 0; i < 10; i++)
+	int sum = 0;
+	double avg = 0;
+	srand(time(NULL));
+
+	//³­¼ö ¹ÞÀ½
+	for (int i = 0; i < sizeof(a)/sizeof(int); i++)
 	{
 		a[i]= rand() % 100 + 1;
 		printf("%d ", a[i]);
-	}
-
-	int sum = 0;
-	double avg = 0;
-
-	for (int i = 0; i < 10; i++)
-	{
 		sum += a[i];
 	}
 
+	//Æò±Õ
 	avg = (double)sum / (sizeof(a) / sizeof(int));
 
 	printf("ÇÕ°è : %d, Æò±Õ : %.1f", sum, avg);
+
+	return 0;
 }
