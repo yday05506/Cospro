1593bb90ca5fd41202fa607f21fbd2849caaf1d6
yday <yangdayeon2@gmail.com>
2022-04-01 12:00:28 +0900

    배열 복사

--- StringSize.c
+++ StringSize.c
@@ -3,6 +3,7 @@
 int main(void)
 {
 	char s1[20] = { "coding test" };
+	char s2[20];
 	
 	int i, j;
 	int cnt = 0;	//0, \0이 나올 때까지
@@ -21,5 +22,11 @@ int main(void)
 			break; 
 		cnt++;
 	}
-	printf("문자열의 길이 : %d", cnt);
+	printf("문자열의 길이 : %d\n", cnt);
+
+	for (i = 0; i <= cnt; i++)
+	{
+		s2[i] = s1[i];
+	}
+	printf("복사본 : %s", s2);
 }
