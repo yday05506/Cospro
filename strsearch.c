5246f10eb03056fa720e353f43709718699031fa
yday <yangdayeon2@gmail.com>
2022-04-08 11:13:02 +0900

    배열 안에 있는 '독일'을 검색하고 몇 번째에 있는지 반환

---
+++ strsearch.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void) {
+	// s1에서 독일이 있는지 검색하고, 있다면 몇 번째인지 반환하는 프로그램
+	// 이때 독일이 여러 개 있다면 모두 검색
+	char s1[7][10] = { "한국", "미국", "일본", "영국", "독일", "호주", "독일" };
+	char key[] = { "독일" };
+	int i, j;
+	// int len = strlen(s1);
+
+	for (i = 0; i < 7; i++) {
+		printf("%s\n", s1[i]);
+		if (strcmp(key, s1[i]) == 0) {
+			printf("배열 안 독일 위치 : %d번째\n", i + 1);
+			break;
+		}
+	}
+
+	return 0;
+}
